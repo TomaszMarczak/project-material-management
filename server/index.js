ad3bbaf0ea43e789b69176695bbd419fc22caec9
Tomasz Marczak <tommy.marczak@gmail.com>
2022-08-29 21:58:55 +0200

    Established database connection

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("./config/db");
 const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+connectDB();
 // app.use(cors);
 app.use(
   "/graphql",
